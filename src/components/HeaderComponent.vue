504d75d7620f9c04e2c0b69d1d4b175d5a112a00
GianlucaGiovannini <gianluca-giova@hotmail.it>
2022-05-03 14:45:07 +0200

    Initial Commit

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <header>
+        prova
+    </header>
+</template>
+
+<script>
+
+export default {
+    name: 'HeaderComponent'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/variables';
+
+</style>
