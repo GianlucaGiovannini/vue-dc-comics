5948f1b8716bf965300109a4013bbf198ca58c97
GianlucaGiovannini <gianluca-giova@hotmail.it>
2022-05-03 17:55:17 +0200

    commit

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,7 +1,149 @@
 <template>
     <footer>
-        <div class="container">
-            footer
+        <div class="footer_top py-5">
+            <div class="container position-relative">
+                <div class="row row-cols-2">
+                    <div class="col">
+                        <div class="row row-cols-3">
+                            <div class="col">
+                                <h4 class="text-white py-3">DC Comics</h4>
+                                <ul>
+                                    <li>
+                                        <a href="#">Characters</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Comics</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Movies</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">TV</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Games</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Videos</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Characters</a>
+                                    </li>
+                                </ul>
+                                <h4 class="text-white py-3">SHOP</h4>
+                                <ul>
+                                    <li>
+                                        <a href="#">Shop DC</a>
+                                        <a href="#">Shop DC Collectibles</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="col">
+                                <h4 class="text-white py-3">DC</h4>
+                                <ul>
+                                    <li>
+                                        <a href="#">Terms Of Use</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Privacy policy (New)</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Ad Choices</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Advertising</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Jobs</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Subscription</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Talent Workshops</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">CPSC Certificates</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Ratings</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Shop Help</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Contact Us</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="col">
+                                <h4 class="text-white py-3">SITES</h4>
+                                <ul>
+                                    <li>
+                                        <a href="#">DC</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">MAD Magazine</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">DC Kids</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">DC Universe</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">DC Power Visa</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <img class="img_footer_logo" src="@/assets/img/dc-logo-bg.png">
+                </div>
+            </div>
+        </div>
+        
+        <div class="footer_bottom">
+            <div class="container">
+
+            </div>
         </div>
     </footer>
 </template>
+
+<script>
+export default {
+    name: 'FooterComponent',
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/variables';
+
+footer {
+    .footer_top {
+        background-image: url("@/assets/img/footer-bg.jpg");
+        background-repeat: no-repeat;
+        background-size: cover;
+
+        .img_footer_logo{
+            position: absolute;
+            top: 0;
+            right: 0;
+
+        }
+
+        ul {
+            list-style: none;
+            padding:0;
+        }
+    }
+
+    a {
+        color: $footerTextLi;
+        text-decoration: none;
+    }
+}
+
+</style>
--- src/components/MercBannerComponent.vue
+++ src/components/MercBannerComponent.vue
@@ -1,14 +1,44 @@
 <template>
     <div class="merc_banner">
         <div class="container">
-            <div class="row row-cols-1 row-cols-md-3 row-cols-lg-5 py-4">
-                <div class="col" v-for="({src, text}, index) in mercandise" :key="index">
-                    <a href="#">
-                        <img :src="src" :alt="'merce' + text">
-                        <span>{{ text }}</span>
+            <div class="row row-cols-1 row-cols-md-3 row-cols-xl-5 py-5">
+                <div class="col my-3" v-for="({src, text}, index) in mercandise" :key="index">
+                    <a href="#" class="d-flex align-items-center">
+                        <img :src="require(`@/assets${src}`)" :alt="'merce' + text">
+                        <span class="ms-2 text-uppercase">{{ text }}</span>
                     </a>
                 </div>
-                <img src="@/assets/img/buy-comics-digital-comics.png" alt="">
+                <!-- <div class="col my-3 p-0">
+                    <a href="#" class="d-flex align-items-center">
+                        <img src="@/assets/img/buy-comics-digital-comics.png">
+                        <span class="ms-2 text-uppercase">digital comics</span>
+                    </a>
+                </div>
+                <div class="col my-3 p-0">
+                    <a href="#" class="d-flex align-items-center">
+                        <img src="@/assets/img/buy-comics-merchandise.png">
+                        <span class="ms-2 text-uppercase">Dc Merchandise</span>
+                    </a>
+                </div>
+                <div class="col my-3 p-0">
+                    <a href="#" class="d-flex align-items-center">
+                        <img src="@/assets/img/buy-comics-subscriptions.png">
+                        <span class="ms-2 text-uppercase">subscription</span>
+                    </a>
+                </div>
+                <div class="col my-3 p-0">
+                    <a href="#" class="d-flex align-items-center">
+                        <img src="@/assets/img/buy-comics-shop-locator.png">
+                        <span class="ms-2 text-uppercase">comic shop locator</span>
+                    </a>
+                </div>
+                <div class="col my-3 p-0">
+                    <a href="#" class="d-flex align-items-center">
+                        <img src="@/assets/img/buy-dc-power-visa.svg">
+                        <span class="ms-2 text-uppercase">dc power visa</span>
+                    </a>
+                </div> -->
+
                 
             </div>
         </div>
@@ -22,23 +52,23 @@ export default {
         return {
             mercandise: [
                 {
-                    src: "@/assets/img/buy-comics-digital-comics.png",
+                    src: "/img/buy-comics-digital-comics.png",
                     text: "digital comics"
                 },
                 {
-                    src: "@/assets/img/buy-comics-merchandise.png",
+                    src: "/img/buy-comics-merchandise.png",
                     text: "Dc Merchandise"
                 },
                 {
-                    src: "@/assets/img/buy-comics-subscriptions.png",
+                    src: "/img/buy-comics-subscriptions.png",
                     text: "subscription"
                 },
                 {
-                    src: "@/assets/img/buy-comics-shop-locator.png",
+                    src: "/img/buy-comics-shop-locator.png",
                     text: "comic shop locator"
                 },
                 {
-                    src: "@/assets/img/buy-dc-power-visa.svg",
+                    src: "/img/buy-dc-power-visa.svg",
                     text: "dc power visa"
                 },
             ],
@@ -51,11 +81,19 @@ export default {
 @import '@/assets/scss/variables';
 .merc_banner {
     background-color: $primaryColor;  
-     
+    
+    img {
+            width: auto;
+            max-height: 55px;
+        }
 
     a {
         color: $bannerTextColor; 
         text-decoration: none;
+
+       span {
+           font-size: 0.8rem;
+       }
     }
 }
 </style>
